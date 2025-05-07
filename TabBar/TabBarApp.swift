070e7423a5621ae8ce6ccda3ee49e39af588453b
GaliSrikanth <GaliSrikanth19@gmail.com>
2025-05-07 12:45:23 +0530

    Initial Commit

---
+++ TabBar/TabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarApp.swift
+//  TabBar
+//
+//  Created by GaliSrikanth on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
